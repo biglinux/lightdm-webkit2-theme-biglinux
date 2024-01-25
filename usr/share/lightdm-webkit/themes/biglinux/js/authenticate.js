0463e3d4bc1e4138e4a424df8b767a42fed161f0
talesam <talesam@gmail.com>
2024-01-24 21:43:26 -0300

    Fix authenticate.js

--- usr/share/lightdm-webkit/themes/biglinux/js/authenticate.js
+++ usr/share/lightdm-webkit/themes/biglinux/js/authenticate.js
@@ -1,87 +1,133 @@
 class Authenticate {
-    constructor() {
-        this._passwordBox = document.querySelector('#input-password-box');
-        this._passwordInput = document.querySelector('#input-password');
-        this._buttonAuthenticate = document.querySelector('#button-authenticate');
-        this._passwordInputContainer = document.querySelector('#input-password-container');
-        this._tooltipPassword = document.querySelector('#tooltip-password');
-        this._password = '';
-        this._init();
-    }
+	constructor() {
+		this._passwordBox = document.querySelector('#input-password-box');
+		this._passwordInput = document.querySelector('#input-password');
+		this._buttonAuthenticate = document.querySelector('#button-authenticate');
+		this._passwordInputContainer = document.querySelector('#input-password-container');
+		this._tooltipPassword = document.querySelector('#tooltip-password');
+		this._password = '';
+		this._init();
+	}
 
-    // Start authentication
-    startAuthentication() {
-        lightdm.cancel_authentication();
-        lightdm.authenticate(String(accounts.getDefaultUserName()));
-    }
+	// Start authentication
+	startAuthentication() {
+		lightdm.cancel_authentication();
+		lightdm.authenticate(String(accounts.getDefaultUserName()));
+	}
 
-    // Timer expired, create new authentication session
-    _autologinTimerExpired() {
-        window.autologin_timer_expired = () => {
-            this.startAuthentication();
-        };
-    }
+	// Timer expired, create new authentication session
+	_autologinTimerExpired() {
+		window.autologin_timer_expired = () => {
+			this.startAuthentication();
+		};
+	}
 
-    // Authentication completed callback
-    _authenticationComplete() {
-        window.authentication_complete = () => {
-            if (lightdm.is_authenticated) {
-                this._authenticationSuccess();
-            } else {
-                // If you want to handle authentication failure, add your code here
-            }
-        };
-    }
+	// Authentication completed callback
+	_authenticationComplete() {
+		window.authentication_complete = () => {
+			if (lightdm.is_authenticated) {
+				this._authenticationSuccess();
+			} else {
+				this._authenticationFailed();
+			}
+		};
+	}
 
-    // You passed to authentication
-    _authenticationSuccess() {
-        this._password = null;
-        this._passwordInput.readOnly = true;
-        this._passwordInput.blur();
-        this._passwordBox.classList.add('authentication-success');
+	// You passed to authentication
+	_authenticationSuccess() {
+		this._password = null;
 
-        setTimeout(() => {
-            lightdm.start_session_sync(String(sessions.getDefaultSession()));
-        }, 1000);
-    }
+		// Make password input read-only
+		this._passwordInput.readOnly = true;
+		this._passwordInput.blur();
+		
+		// Success messages
+		this._passwordBox.classList.add('authentication-success');
 
-    // Register keyup event
-    _buttonAuthenticateClickEvent() {
-        this._buttonAuthenticate.addEventListener(
-            'click',
-            () => {
-                this._password = this._passwordInput.value;
-                lightdm.respond(String(this._password));
-            }
-        );
-    }
+		setTimeout(
+			() => {
+				loginFade.showLoginFade();
+			},
+			500
+		);
 
-    // Register keydown event
-    _passwordInputKeyDownEvent() {
-        this._passwordInput.addEventListener(
-            'keydown',
-            e => {
-                this._password = this._passwordInput.value;
-                if (e.key === 'Enter') {
-                    lightdm.respond(String(this._password));
-                }
-            }
-        );
-    }
+		// Add a delay before unlocking
+		setTimeout(
+			() => {
+				this._buttonAuthenticate.classList.remove('authentication-success');
+				lightdm.start_session_sync(String(sessions.getDefaultSession()));
+			},
+			1000
+		);
+	}
 
-    _init() {
-        this._autologinTimerExpired();
-        this._authenticationComplete();
-        this._buttonAuthenticateClickEvent();
-        this._passwordInputKeyDownEvent();
-        if (!lightdm) {
-            lightdm.onload = function() {
-                console.log('Start authentication');
-                this.startAuthentication();
-            };
-        } else {
-            console.log('Start authentication');
-            this.startAuthentication();
-        }
-    }
+	// Remove authentication failure messages
+	_authFailedRemove() {
+		this._passwordBox.classList.remove('authentication-failed');
+	}
+
+	// You failed to authenticate
+	_authenticationFailed() {
+		this._password = null;
+
+		// New authentication session
+		this.startAuthentication();
+		this._passwordInput.value = '';
+
+		// Error messages/UI
+		this._passwordBox.classList.add('authentication-failed');
+
+		// Shake animation
+		this._passwordInputContainer.classList.add('shake');
+		setTimeout(
+			() => {
+				// Stop shaking
+				this._passwordInputContainer.classList.remove('shake');
+			},
+			500
+		);
+	}
+
+	// Register keyup event
+	_buttonAuthenticateClickEvent() {
+		this._buttonAuthenticate.addEventListener(
+			'click',
+			() => {
+				console.log(lightdm.in_authentication);
+				this._authFailedRemove();
+				this._password = this._passwordInput.value;
+				lightdm.respond(String(this._password));
+			}
+		);
+	}
+
+	// Register keydown event
+	_passwordInputKeyDownEvent() {
+		this._passwordInput.addEventListener(
+			'keydown',
+			e => {
+				this._authFailedRemove();
+				this._password = this._passwordInput.value;
+				if (e.key === 'Enter') {
+					lightdm.respond(String(this._password));
+				}
+			}
+		);
+	}
+
+	_init() {
+		this._autologinTimerExpired();
+		this._authenticationComplete();
+		this._buttonAuthenticateClickEvent();
+		this._passwordInputKeyDownEvent();
+		if (!lightdm) {
+			lightdm.onload = function() {
+				console.log('Start authentication');
+				this.startAuthentication();
+			};
+		} else {
+			console.log('Start authentication');
+			this.startAuthentication();
+		}
+	}
 }
